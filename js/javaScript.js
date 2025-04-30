ed363da6ef4e9a7245d360a3e0b4d25f466f9095
NicolyMarquesLemes <nicoly.lemes08@gmail.com>
2025-04-30 16:57:19 -0300

    exercicio em andamento

--- js/javaScript.js
+++ js/javaScript.js
@@ -8,16 +8,32 @@ const Casa = document.querySelector("div");
 Casa.style.backgroundColor = "blue";
 
 const botao = document.getElementById("botao");
-const frm = document.querySelector("form");
+const frm = document.querySelector("#btn");
+
 frm.addEventListener("submit", (e) => {
-botao.outerText = "Clicado!"
+botao.outerText = "Clicado!";
 e.preventDefault();
-});
+})
 
 const dive = document.getElementById("caixa");
 dive.innerHTML = "<strong>Conteúdo em negrito</strong>";
 
-const big = document.querySelector ("p")
+const big = document.querySelector ("p");
 big.style.color = "green";
 big.style.fontSize = "20px";
 big.style.backgroundColor = "yellow";
+
+const Pestilo = document.getElementById("estilo");
+const Frm = document.querySelector("#BTN");
+
+Frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Pestilo.classList.add("estilizar");
+})
+
+const rmv = document.getElementById("Btn");
+rmv.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Pestilo.classList.remove("estilizar");
+})
+
